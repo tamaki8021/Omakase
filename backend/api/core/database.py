79b7bea327d7b5fabe7ea28e651e900628fdfd95
tamaki8021 <hbk2002320@gmail.com>
2023-11-27 11:18:37 +0900

    feat: Generate init Alembic migration file

--- backend/api/core/database.py
+++ backend/api/core/database.py
@@ -1,13 +1,21 @@
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-from env import SQLALCHEMY_DATABASE_URL
+from api.env import SQLALCHEMY_DATABASE_URL, SQLALCHEMY_ASYNC_DATABASE_URL
 
+# 同期エンジン（Alembic用）
+sync_engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-async_engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+# 非同期エンジン（FastAPI用）
+async_engine = create_async_engine(SQLALCHEMY_ASYNC_DATABASE_URL, echo=True)
 async_session = sessionmaker(autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession)
 
 Base = declarative_base()
 
 async def get_db():
-    async with async_session() as session:
-        yield session
+    try:
+        db = async_session()
+        yield db
+    finally:
+        if db:
+            await db.close()
--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -3,13 +3,17 @@ import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+
+from api.env import SQLALCHEMY_DATABASE_URL
+import api.models as models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_section_option("alembic", "SQLALCHEMY_DATABASE_URL", os.environ.get("SQLALCHEMY_DATABASE_URL"))
+
+# connect to database
+config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
