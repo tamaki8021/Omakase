3d84b5ebe010d0dab0ee9a44575783acd5e40ad0
tamaki8021 <hbk2002320@gmail.com>
2023-12-05 16:31:42 +0900

    fix: warningの対応

--- backend/api/schemas/task.py
+++ backend/api/schemas/task.py
@@ -3,14 +3,14 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 class TaskBase(BaseModel):
-    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+    title: Optional[str] = Field(None, json_schema_extra="クリーニングを取りに行く")
 
 class Task(TaskBase):
     id: int
     done: bool = Field(False, description="完了フラグ")
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class TaskCreate(TaskBase):
     pass
@@ -20,4 +20,4 @@ class TaskCreateResponse(TaskCreate):
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
